e730e62f4b16d5c5812a0018f5d20762c170e235
Joseph Gonzalez <joseph220199@gmail.com>
2021-12-14 13:32:44 -0600

    Added Instructions to snippets

--- Ficheros/Files_TXT.java
+++ Ficheros/Files_TXT.java
@@ -6,6 +6,10 @@ import java.io.FileWriter;
 
 public class Files_TXT 
 {
+    /*
+        ESTE MÉTODO TE PERMITE CREAR UN ARCHIVO DE TEXTO CON UN NOMBRE ESPECÍFICO.
+        EJEMPLO: createNewFile("nombres.txt");
+    */
     public void createNewFile(String nombreArchivo)
     {
         try
@@ -22,6 +26,10 @@ public class Files_TXT
         }
     }
 
+    /*
+        ESTE MÉTODO TE PERMITE ELIMINAR UN ARCHIVO DE TEXTO CON UN NOMBRE ESPECÍFICO.
+        EJEMPLO: deleteFile("nombres.txt");
+    */
     public void deleteFile(String nombreArchivo)
     {
         try
@@ -37,6 +45,41 @@ public class Files_TXT
             System.out.println("Error al eliminar archivo: " + e.getMessage());
         }
     }
+
+    /*
+        ESTE MÉTODO TE PERMITE IMPRIMIR EN CONSOLA UN ARCHIVO DE TEXTO CON UN NOMBRE ESPECÍFICO.
+        EJEMPLO: readFile("nombres.txt");
+    */
+
+    public void readFile(String nombreArchivo)
+    {
+        try
+        {
+            File archivo = new File(nombreArchivo);
+            if (archivo.exists())
+            {
+                FileReader fr = new FileReader(archivo);
+                BufferedReader br = new BufferedReader(fr);
+                String linea;
+                while ((linea = br.readLine()) != null)
+                {
+                    System.out.println(linea);
+                }
+                br.close();
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error al leer archivo: " + e.getMessage());
+        }
+    }
+
+    /*
+        ESTE MÉTODO TE PERMITE SOBREESCRIBIR UN ARCHIVO DE TEXTO CON UN CONTENIDO ESPECÍFICO.
+        EJEMPLO 1: rewriteFile("nombres.txt", "Juan\nPedro\nJavier\n");
+        EJEMPLO 2: String nombres = "Juan\nPedro\nJavier\n";
+                    rewriteFile("nombres.txt", nombres);
+    */
 
     public void rewriteFile(String nombreArchivo, String contenido)
     {
@@ -57,6 +100,13 @@ public class Files_TXT
             System.out.println("Error al sobreescribir archivo: " + e.getMessage());
         }
     }
+
+    /*
+        ESTE MÉTODO TE PERMITE EDITAR UN ARCHIVO DE TEXTO CON UN NOMBRE ESPECÍFICO AÑADIENDO EL CONTENIDO AL FINAL.
+        EJEMPLO 1: editFile("nombres.txt", "Pablo\nRodrigo\nLuis\n");
+        EJEMPLO 2: String nombres = "Pablo\nRodrigo\nLuis\n";
+                    editFile("nombres.txt", nombres);
+    */
 
     public void editFile(String nombreArchivo, String contenido)
     {
